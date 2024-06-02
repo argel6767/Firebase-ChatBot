4b019c419820e30f9fa2d61e556969669f4a5164
argel6767 <argel6767@gmail.com>
2024-06-02 01:29:27 -0400

    addUser.js in progress

--- functions/index.js
+++ functions/index.js
@@ -3,4 +3,7 @@ admin.initializeApp();
 
 const {addMessage} = require('./api/addmessage');
 
+const {addUser} = require("./api/addUser");
+
 exports.addMessage = addMessage;
+exports.addUser = addUser;
