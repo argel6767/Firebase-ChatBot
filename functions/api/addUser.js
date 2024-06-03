102f86afaea7ecf2a9b04267bd40e679fd97ae19
Argel Hernandez Amaya <argelhernandezamaya@host-Argels-Air-mac-147dda22a2f3.eduroam.ssid.10net.amherst.edu>
2024-06-02 21:12:01 -0400

    finish addUser cloud function

--- functions/api/addUser.js
+++ functions/api/addUser.js
@@ -2,27 +2,27 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const { logger } = functions;
 
-// Initialize Firebase Admin if not already initialized
-if (!admin.apps.length) {
+
     admin.initializeApp();
-}
+
 
 const db = admin.firestore();
 
 exports.addUser = functions.https.onCall(async (data, context) => {
-    try {
-        const { userId } = data;
-    
-        if (!userId) {
-          throw new functions.https.HttpsError('invalid-argument', 'User ID is required.');
-        }
-    
-        // Create a new document with no fields under the chats collection
-        await db.collection('chats').doc(userId).set({});
-    
-        return { message: 'User added to chat successfully!' };
-      } catch (error) {
-        console.error('Error adding user to chat:', error);
-        throw new functions.https.HttpsError('internal', 'Something went wrong!');
-      }
-    });
+  try {
+    const { userId } = data;
+
+    if (!userId) {
+        throw new functions.https.HttpsError('invalid-argument', 'User ID is required.');
+    }
+
+    // Create a new document with no fields under the chats collection
+    const userRef = db.collection('chats');
+    await admin.firestore().add(userId);
+
+    return { message: 'User added to chat successfully!' };
+} catch (error) {
+    console.error('Error adding user to chat:', error);
+    throw new functions.https.HttpsError('internal', 'Something went wrong!');
+}
+});
